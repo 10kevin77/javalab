c00745495500a14396bc41a7d68f39e8fa3da6fe
10kevin77 <145546452+10kevin77@users.noreply.github.com>
2023-09-26 15:20:34 +0530

    Grades

---
+++ grades.java
@@ -0,0 +1,60 @@
+/***************************************************************************************************
+ * File          :   grades.java
+ * Description   :   Find Grades
+ * Author        :   Kevin Biju Kulangara
+ * Version       :   1.0
+ * Date          :   26/09/2004
+ *************************************************************************************************/
+
+package sample;
+
+import java.util.Scanner;
+
+public class grades {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter marks");
+		int mark=sc.nextInt();
+		
+		if(mark>90) {
+			System.out.println("The grade is S");
+		}
+		else if(mark>85){
+			System.out.println("The grade is A+");
+		}
+		else if(mark>80){
+			System.out.println("The grade is A");
+		}
+		else if(mark>75){
+			System.out.println("The grade is B+");
+		}
+		else if(mark>70){
+			System.out.println("The grade is B");
+		}
+		else if(mark>65){
+			System.out.println("The grade is C+");
+		}
+		else if(mark>60){
+			System.out.println("The grade is C");
+		}
+		else if(mark>55){
+			System.out.println("The grade is D+");
+		}
+		else if(mark>50){
+			System.out.println("The grade is D");
+		}
+		else if(mark>45){
+			System.out.println("The grade is E+");
+		}
+		else if(mark>40){
+			System.out.println("The grade is E");
+		}
+		else {
+			System.out.println("The grade is F");
+		}
+	}
+
+}
