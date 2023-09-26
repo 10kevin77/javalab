1124211f42281142dee2ca818a2435edbd9a6a83
10kevin77 <145546452+10kevin77@users.noreply.github.com>
2023-09-26 15:05:57 +0530

    attendance mark

---
+++ Attendance.java
@@ -0,0 +1,37 @@
+/***************************************************************************************************
+ * File          :   Attendance.java
+ * Description   :   Find attendance percentage
+ * Author        :   Kevin Biju Kulangara
+ * Version       :   1.0
+ * Date          :   26/09/2004
+ *************************************************************************************************/
+
+
+
+
+
+package sample;
+
+import java.util.Scanner;
+
+public class Attendance {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter attendance percentage");
+		
+		float attendance=sc.nextInt();
+		
+		if(attendance>90) {
+			System.out.println("attendance mark=10");
+			
+		}
+		else {
+			attendance=attendance/10;
+			System.out.println("attendance mark="+attendance);
+		}
+		
+	}
+
+}
